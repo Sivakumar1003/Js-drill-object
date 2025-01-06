6bedb2ea2420bd353c323760dbc88aa2c6fce6af
Sivakumar N <shivask94423@gmail.com>
2025-01-06 12:35:09 +0530

    test file for calling pairs function.

---
+++ Test/testPairs.js
@@ -0,0 +1,7 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+
+const pairs = require('../pairs.js');
+
+// calling pairs function.
+const result = pairs(testObject);
+console.log(result);
