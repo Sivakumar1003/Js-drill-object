6ec605767f802de8df7600bb9ba8486d4886ab4b
Sivakumar N <shivask94423@gmail.com>
2025-01-06 12:34:01 +0530

    test file for calling invert function.

---
+++ Test/testInvert.js
@@ -0,0 +1,7 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+
+const invert = require('../invert.js');
+
+// calling invert function.
+const result = invert(testObject);
+console.log(result);
